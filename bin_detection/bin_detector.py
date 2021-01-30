1bf5349e5b4ca285d0a4cfd33c408913cb15d8f2
Ian Pegg <ianpegg9@gmail.com>
2021-01-29 16:06:48 -0800

    Bounding works on validation data

--- bin_detection/bin_detector.py
+++ bin_detection/bin_detector.py
@@ -5,10 +5,15 @@ import pickle
 
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 from skimage.measure import label, regionprops
 
 from regression import Regression
 from bin_detection.data_loader import DataLoader
+
+MAX_WIDTH_RATIO = 2.5
+MIN_WIDTH_RATIO = 1.2
+MIN_AREA = 1000
 
 
 class BinDetector:
@@ -53,9 +58,27 @@ class BinDetector:
             boxes - a list of lists of bounding boxes. Each nested list is a bounding box in the form of [x1, y1, x2, y2] 
             where (x1, y1) and (x2, y2) are the top left and bottom right coordinate respectively
         """
-        # YOUR CODE HERE
-        x = np.sort(np.random.randint(img.shape[0], size=2)).tolist()
-        y = np.sort(np.random.randint(img.shape[1], size=2)).tolist()
-        boxes = [[x[0], y[0], x[1], y[1]]]
-        boxes = [[182, 101, 313, 295]]
-        return boxes
+        blur_kernel_size = (30, 30)
+        img = cv2.blur(img.astype(float), blur_kernel_size) > 0.8
+        labeled = label(img, background=0, connectivity=2)
+        props = regionprops(labeled)
+        self.plot(labeled)
+
+        bounding_boxes = []
+        for p in props:
+            width = abs(p.bbox[1] - p.bbox[3])
+            height = abs(p.bbox[0] - p.bbox[2])
+            if p.area < MIN_AREA:
+                continue
+            elif width > height * MIN_WIDTH_RATIO:
+                continue
+            elif height > width * MAX_WIDTH_RATIO:
+                continue
+            else:
+                bounding_boxes.append(p.bbox)
+        return bounding_boxes
+
+    def plot(self, img):
+        fig, axs = plt.subplots()
+        axs.imshow(img)
+        plt.show()
--- bin_detection/test.py
+++ bin_detection/test.py
@@ -4,6 +4,7 @@ from typing import Union, Tuple, List
 
 import cv2
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 
 from bin_detection.data_loader import DataLoader
@@ -15,6 +16,7 @@ VAL_DATA_DIR = 'bin_detection/data/validation'
 VAL_MASK_DIR = 'bin_detection/data/masks/validation'
 
 
+@unittest.skip
 class TestSegmentImages(unittest.TestCase):
     def setUp(self) -> None:
         self.bd = BinDetector()
@@ -33,6 +35,48 @@ class TestSegmentImages(unittest.TestCase):
         axs[0].imshow(img)
         axs[1].imshow(mask)
         plt.show()
+
+
+class TestBinDetection(unittest.TestCase):
+    def setUp(self) -> None:
+        self.bd = BinDetector()
+        self.images = [
+            DataLoader.load_img(VAL_DATA_DIR, img_file) for img_file in os.listdir(VAL_DATA_DIR)
+        ]
+
+    def test_bound_bin(self):
+        for img in self.images:
+            mask = self.bd.segment_image(img)
+            bounding_boxes = self.bd.get_bounding_boxes(mask)
+            print(bounding_boxes)
+            self.assertIsInstance(bounding_boxes, list)
+            for box in bounding_boxes:
+                self.assertIsInstance(box, tuple)
+                for element in box:
+                    self.assertIsInstance(element, int)
+
+            self.plot(img, mask, bounding_boxes)
+
+    def plot(self, img: Union[np.ndarray, int], mask: np.ndarray, bounding_boxes: List[List[int]]):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        fig, axs = plt.subplots(1, 2)
+        axs[0].imshow(img)
+        axs[1].imshow(mask)
+
+        for ax in axs:
+            for box in bounding_boxes:
+                rect = self.get_rect(box)
+                ax.add_patch(rect)
+
+        plt.show()
+
+    @staticmethod
+    def get_rect(box: List[int]):
+        width = abs(box[1] - box[3])
+        height = abs(box[0] - box[2])
+        anchor = (box[1], box[2] - height)
+        rect = patches.Rectangle(anchor, width, height, linewidth=1, edgecolor='r', facecolor='none')
+        return rect
 
 
 if __name__ == '__main__':
